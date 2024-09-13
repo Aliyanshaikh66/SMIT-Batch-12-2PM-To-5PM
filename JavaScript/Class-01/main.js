b4beaa316dac954a6007c0f793b7ad47c4f2322e
aliyanshaikh66 <aliyanzahid02@gmail.com>
2024-09-13 17:05:41 +0500

    js

---
+++ JavaScript/Class-01/main.js
@@ -0,0 +1,16 @@
+// alert("Are You Ready");
+
+// a = 10;
+// b = 20;
+// c = a + b; 
+// console.log(c);
+
+// var a = 40;
+// var b = 60;
+// let c = a * b;
+// console.log (c)
+
+
+var weight = 150;
+weight = weight + 25; 
+console.log (weight + 25)
