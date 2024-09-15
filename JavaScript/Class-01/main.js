892291eb014a8fc961ebdf8a6cd8d9cdefa7d4aa
aliyanshaikh66 <aliyanzahid02@gmail.com>
2024-09-15 16:45:19 +0500

    Chapter 5 to 9 Complete

--- JavaScript/Class-01/main.js
+++ JavaScript/Class-01/main.js
@@ -11,6 +11,54 @@
 // console.log (c)
 
 
-var weight = 150;
-weight = weight + 25; 
-console.log (weight + 25)
+// var weight = 150;
+// weight = weight + 25; 
+// console.log (weight + 25)
+
+// var a = 5;
+// var b = 5;
+// var c  ;
+// console.log(c);
+
+
+// document.write("<h1>Hello World</h1>");
+
+// var num = 50;
+// var b = num + 500;
+// console.log(b);
+
+// var num = 1;
+// var b = num ;
+// console.log(b);
+
+// var num = (13)*(2) + 5 - 1;
+// console.log(num);
+
+// var msg = "Thank For your Input !";
+// alert(msg);
+// var name = "Mossa";
+// alert("Thanks, " + name  + "!");
+
+// alert("thanks" + username + "!")
+
+
+// Simple Calculator
+
+// var msg1 = prompt("Enter your 1st Number");
+// var msg3 = Number(msg1);
+// var msg2 = prompt("Enter your 2nd Number");
+// var msg4 = Number(msg2);
+// alert(msg3 / msg4);
+
+
+
+
+
+
+// console.log(msg3 * msg4);
+
+
+
+
+
+
