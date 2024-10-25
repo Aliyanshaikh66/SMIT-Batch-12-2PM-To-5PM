628a1e5819bbad1b5d3bc6920aa8990cc9d09283
AliyanShaikh66 <aliyanzahid02@gmail.com>
2024-10-25 17:10:15 +0500

    add

--- JavaScript/Chapter-26/main.js
+++ JavaScript/Chapter-26/main.js
@@ -8,3 +8,44 @@
 
 // var result = Math.floor(2);
 // console.log(result);
+
+
+// var num = 2.5;
+// console.log(Math.round(num));
+// var num = 2.1;
+// console.log(Math.ceil(num));
+// var num = 2.99;
+// console.log(Math.floor(num));
+
+// var num = "";
+// var num1 = (Math.random(num)*50000+1);
+// var round = (Math.round(num1));
+// console.log(round);
+
+// var profit = "200" - "cat"
+// console.log(profit);
+
+// var age = 2.6;
+// var findage = age + 1;
+// console.log(findage);
+
+// var num = parseInt("2.0525");
+// console.log(num);
+
+// var str = "2.52";
+// console.log (str.toString());
+
+// var item = 180;
+// var totalprice = item * 1.5064;
+// var result = totalprice.toFixed(16);
+// console.log(result);
+
+
+// var num = 5;
+// var str = num.toString();
+//  if (str.charAt(str.length - 1) === "5") {
+//    str = str.slice(0, str.length - 1) + "6";
+// }
+//  num = Number(str);
+//  prettyNum = num.toFixed(3)
+//  console.log(prettyNum);
